aa81d232d755c028faa1ad3b28f3985af18bf530
Heritiana <heritiana.ratsimbazafy99@gmail.com>
2026-04-19 19:32:07 +0300

    Correction du trigger SQL Supabase

--- supabase/schema-v1.sql
+++ supabase/schema-v1.sql
@@ -224,7 +224,7 @@ $$;
 drop trigger if exists on_auth_user_created on auth.users;
 create trigger on_auth_user_created
 after insert on auth.users
-for each row execute procedure public.handle_new_user();
+for each row execute function public.handle_new_user();
 
 create or replace function public.is_admin()
 returns boolean
